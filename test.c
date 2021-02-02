8102fb446d248ef16233f939122d09c5b31519cb
caodewei <916894337@qq.com>
2021-02-02 23:28:45 +0800

    add test.c

---
+++ test.c
@@ -0,0 +1,18 @@
+/*================================================================
+* File Name：test.c
+* Creator：CDW
+* Dete Created：2021.02.02
+* Describe：
+================================================================*/
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	printf("hello world!\n");	
+	
+	return 0;
+}
+
